bb748e59512f4ebe02047e0798d59061dd921309
yyedcy <liuyihan@hu-berlin.de>
2016-08-13 21:39:02 +0800

    Update FAMPCA.R

--- FAMPCA/FAMPCA.R
+++ FAMPCA/FAMPCA.R
@@ -11,6 +11,6 @@ bip1=b i p l o t ( pca . time , c h o i c e s =1:2)
 
 #  generate interactive plot
 l ibrary ( r g l )
-plot3d ( pca . time$ s c o r e s [ , 1 : 3 ] )
+plot3d ( pca . time$ s c o r e s [, 1 : 3 ] )
 l ibrary ( pca3d )
 pca3d ( pca . time , components = 1 : 3 )
