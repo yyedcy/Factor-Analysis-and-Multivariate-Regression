a0acea8625fd011e661e377849367868b36d2030
yyedcy <liuyihan@hu-berlin.de>
2016-08-13 20:32:03 +0800

    Update FAMmultivariateregression.R

--- FAMmultivariateregression/FAMmultivariateregression.R
+++ FAMmultivariateregression/FAMmultivariateregression.R
@@ -1,7 +1,7 @@
 
 #  build a full model and check
 attach (Timebudget2 )
-Model1 <􀀀 lm(TELE ~ PROF+TRAN+HOUS+KIDS+SHOP+PERS+EAT+SLEE+LEAS,
+Model1 = lm(TELE ~ PROF+TRAN+HOUS+KIDS+SHOP+PERS+EAT+SLEE+LEAS,
 data=Timebudget )
 l ibrary ( ' fmsb ' )
 VIF(Model1 )
@@ -32,7 +32,7 @@ o u t l i e rTe s t (Model2 )
 i n f l u e n c ePl o t (Model2 , id .method=" i d e n t i f y " , main=" I n f l u e n c e Plot " , sub=" Ci r c l e s
 
 #  prove the normality of residual
-library (MASS)
+l i b r a r y (MASS)
 sresid = s tudr e s (Model2 )
 hist ( s r e s i d , f r e q=FALSE, main=" Di s t r i b u t i o n o f Student i z ed Re s idua l s " , br eaks=10)
 x f i t = seq (min( s r e s i d ) ,max( s r e s i d ) , length=40)
@@ -45,11 +45,11 @@ gvmodel2 = gvlma (Model2 )
 summary( gvmodel2 )
 
 #  add each of the previously discarded variables
-Model3Prof <􀀀 lm(TELE ~ PROF+PERS+EAT+SLEE+LEAS, data=Timebudget )
-Model3Tran <􀀀 lm(TELE ~ TRAN+PERS+EAT+SLEE+LEAS, data=Timebudget )
-Model3Hous <􀀀 lm(TELE ~ HOUS+PERS+EAT+SLEE+LEAS, data=Timebudget )
-Model3Kids <􀀀 lm(TELE ~ KIDS+PERS+EAT+SLEE+LEAS, data=Timebudget )
-Model3Shop <􀀀 lm(TELE ~ SHOP+PERS+EAT+SLEE+LEAS, data=Timebudget )
+Model3Prof = lm(TELE ~ PROF+PERS+EAT+SLEE+LEAS, data=Timebudget )
+Model3Tran = lm(TELE ~ TRAN+PERS+EAT+SLEE+LEAS, data=Timebudget )
+Model3Hous = lm(TELE ~ HOUS+PERS+EAT+SLEE+LEAS, data=Timebudget )
+Model3Kids = lm(TELE ~ KIDS+PERS+EAT+SLEE+LEAS, data=Timebudget )
+Model3Shop = lm(TELE ~ SHOP+PERS+EAT+SLEE+LEAS, data=Timebudget )
 
 #  created manually two dummy variables and regress
 summary( ModelDummy)
