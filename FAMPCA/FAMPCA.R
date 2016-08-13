f1838322948ef0f071106ae9f4d6c9b5e2a98fc0
yyedcy <liuyihan@hu-berlin.de>
2016-08-13 10:53:25 +0800

    Update FAMPCA.R

--- FAMPCA/FAMPCA.R
+++ FAMPCA/FAMPCA.R
@@ -1,16 +1,16 @@
 
 #  principal component analysis
-pca.time=princomp(time , scores=T, cor=T)
-summary(pca . time )
+pca . time=princomp ( time , s c o r e s=T, cor=T)
+summary( pca . time )
 
 #  visualize the rule
-plot(pca . time , type=" l " )
+plot ( pca . time , type=" l " )
 
 #  visualize the vectors of initial variables
-bip1=biplot(pca . time , choices =1:2)
+bip1=b i p l o t ( pca . time , c h o i c e s =1:2)
 
 #  generate interactive plot
-library ( r g l )
+l ibrary ( r g l )
 plot3d ( pca . time$ s c o r e s [ , 1 : 3 ] )
-library ( pca3d )
+l ibrary ( pca3d )
 pca3d ( pca . time , components = 1 : 3 )
